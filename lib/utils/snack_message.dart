66b69cf4dffcd0f8fd2de3bd269601fa3e037f13
Keshav Priyadarshi <git@keshav.space>
2022-10-10 01:29:31 +0530

    redesign snackback
    - fixes https://github.com/keshav-space/safenotes/issues/19
    
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- lib/utils/snack_message.dart
+++ lib/utils/snack_message.dart
@@ -2,10 +2,25 @@
 import 'package:flutter/material.dart';
 
 showSnackBarMessage(BuildContext context, String? message) {
-  if (message != null)
-    ScaffoldMessenger.of(context)
+  if (message != null) {
+    final double width = MediaQuery.of(context).size.width * 0.80;
+
+    return ScaffoldMessenger.of(context)
       ..removeCurrentSnackBar()
       ..showSnackBar(
-        SnackBar(content: Text(message)),
+        SnackBar(
+          width: width,
+          content: Text(
+            message,
+            textAlign: TextAlign.center,
+          ),
+          elevation: 6.0,
+          duration: Duration(milliseconds: 1000),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+          ),
+          behavior: SnackBarBehavior.floating,
+        ),
       );
+  }
 }
