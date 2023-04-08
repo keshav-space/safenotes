491eb3dedc165620312fed3105952f24c6fcd616
Keshav Priyadarshi <git@keshav.space>
2023-04-09 05:22:08 +0530

    Add copyright header
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
+/*
+* Copyright (C) Keshav Priyadarshi and others - All Rights Reserved.
+*
+* SPDX-License-Identifier: GPL-3.0-or-later
+* You may use, distribute and modify this code under the
+* terms of the GPL-3.0+ license.
+*
+* You should have received a copy of the GNU General Public License v3.0 with
+* this file. If not, please visit https://www.gnu.org/licenses/gpl-3.0.html
+*
+* See https://safenotes.dev for support or download.
+*/
+
 // Dart imports:
 import 'dart:async';
 import 'dart:io' show Platform;
--- lib/utils/string_utils.dart
+++ lib/utils/string_utils.dart
@@ -1,3 +1,16 @@
+/*
+* Copyright (C) Keshav Priyadarshi and others - All Rights Reserved.
+*
+* SPDX-License-Identifier: GPL-3.0-or-later
+* You may use, distribute and modify this code under the
+* terms of the GPL-3.0+ license.
+*
+* You should have received a copy of the GNU General Public License v3.0 with
+* this file. If not, please visit https://www.gnu.org/licenses/gpl-3.0.html
+*
+* See https://safenotes.dev for support or download.
+*/
+
 String sanitize(String inputString) {
   final multipleSpaces = RegExp(' +'); // replace with ' '
   final emptylines = RegExp(r'(?:[\t ]*(?:\r?\n|\r))+'); // replace with '\n'
