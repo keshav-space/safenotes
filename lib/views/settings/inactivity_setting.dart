e57cf2446160a4b1b655fada8420f28ab7dd2f91
Keshav Priyadarshi <git@keshav.space>
2023-04-16 23:49:34 +0530

    Remove unused strings
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- lib/views/settings/inactivity_setting.dart
+++ lib/views/settings/inactivity_setting.dart
@@ -39,7 +39,7 @@ class _InactivityTimerSettingState extends State<InactivityTimerSetting> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Inactiviy Timeout'.tr(),
+          'Inactivity Timeout'.tr(),
           style: appBarTitle,
         ),
       ),
